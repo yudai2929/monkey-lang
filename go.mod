f0ae362b9ce2e15be93b3d510e7728f308521a2e
yudai2929 <yudai201729@icloud.com>
2024-09-07 12:26:49 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gtihub.com/yudai2929/monkey-lang
+
+go 1.22
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	// Call the function
+	hello()
+}
+
+func hello() {
+	println("Hello, World!")
+}
